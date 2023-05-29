159bd11ba6305f138a28096b8387c2aa44dec6bf
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-05-29 17:04:16 -0400

    Fixed settings.json

--- src/WebScraperTesting.java
+++ src/WebScraperTesting.java
@@ -7,16 +7,6 @@ import org.jsoup.select.Elements;
 
 class WebScraperTesting {
     public static void main(String[] args){
-        try {
-            Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-
-            String title = doc.title();
-            System.out.println("Title: " + title);
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+        
     }
 }
