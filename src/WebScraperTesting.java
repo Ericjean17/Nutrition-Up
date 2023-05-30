a25460374c8f20e525f749f263b33f24dafede94
Alpacaz4Life <124852072+Alpacaz4Life@users.noreply.github.com>
2023-05-29 20:36:37 -0400

    Base functionality of web scraper

--- src/WebScraperTesting.java
+++ src/WebScraperTesting.java
@@ -6,7 +6,26 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 class WebScraperTesting {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+
+        String start = "https://www.calorieking.com";
+        String mid = "/us/en/foods/search?keywords=";
+        String keyword = "apple"; // will be made based on user input
+        String url = start + mid + keyword;
+
+        Document doc = Jsoup.connect(url).get();
+        Elements elements = doc.select("a");
         
+        String trueUrl = "";
+        int i = 0;
+        for(Element element: elements){
+            if(i == 11){
+                trueUrl = element.attr("href");
+                System.out.println(trueUrl);
+            }
+            i++;
+        }
+
+        System.out.println(start + trueUrl);
     }
 }
