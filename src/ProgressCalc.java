537aa972199ac239776dd9ba16895380244a6c8a
SamuelEChang <samcdec22@gmail.com>
2023-06-12 21:47:37 -0400

    Calorie Goal Tracker Window is Working!!!!!!

--- src/ProgressCalc.java
+++ src/ProgressCalc.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-import javax.swing.JLabel;
+import java.util.ArrayList;
 
 public class ProgressCalc {
     public static double proTotal = 0;
@@ -16,11 +16,11 @@ public class ProgressCalc {
     public static double proProgress = 0;
     public static double fatProgress = 0;
     public static double calProgress = 0;
+    public static String d1, d2, d3, d4, d5, d6, d7;
+    public static ArrayList<Double> calProgressList = new ArrayList<>();
+    public static ArrayList<Double> proProgressList = new ArrayList<>();
+    public static ArrayList<Double> fatProgressList = new ArrayList<>();
     
-
-    
-    
-
     public static void getTotal(){
         String date = FoodDiaryWindow.formattedDate;
         String username = LoginWindow.inputtedUsername;
@@ -74,5 +74,107 @@ public class ProgressCalc {
         calProgress = calTotal / calGoal * 100;
         proProgress = proTotal / proGoal * 100;
         fatProgress = fatTotal / fatGoal * 100;
+    }
+
+    public static void baseArrays(){
+        for (int i = 0; i < 7; i++) {
+            calProgressList.add(0.0);
+            proProgressList.add(0.0);
+            fatProgressList.add(0.0);
+        }
+    }
+
+    public static void progressDates() {
+        String formattedDate = FoodDiaryWindow.formattedDate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate currentDate = LocalDate.parse(formattedDate, formatter);
+        DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
+
+        if (currentDayOfWeek == DayOfWeek.SUNDAY) {
+            System.out.println("Sunday");
+            d7 = currentDate.plusDays(6).toString();
+            d6 = currentDate.plusDays(5).toString();
+            d5 = currentDate.plusDays(4).toString();
+            d4 = currentDate.plusDays(3).toString();
+            d3 = currentDate.plusDays(2).toString();
+            d2 = currentDate.plusDays(1).toString();
+            d1 = currentDate.toString();
+        } else if (currentDayOfWeek == DayOfWeek.MONDAY) {
+            System.out.println("Monday");
+            d7 = currentDate.plusDays(5).toString();
+            d6 = currentDate.plusDays(4).toString();
+            d5 = currentDate.plusDays(3).toString();
+            d4 = currentDate.plusDays(2).toString();
+            d3 = currentDate.plusDays(1).toString();
+            d2 = currentDate.toString();
+            d1 = currentDate.minusDays(1).toString();
+        } else if (currentDayOfWeek == DayOfWeek.TUESDAY) {
+            System.out.println("Tuesday");
+            d7 = currentDate.plusDays(4).toString();
+            d6 = currentDate.plusDays(3).toString();
+            d5 = currentDate.plusDays(2).toString();
+            d4 = currentDate.plusDays(1).toString();
+            d3 = currentDate.toString();
+            d2 = currentDate.minusDays(1).toString();
+            d1 = currentDate.minusDays(2).toString();
+        } else if (currentDayOfWeek == DayOfWeek.WEDNESDAY) {
+            System.out.println("Wednesday");
+            d7 = currentDate.plusDays(3).toString();
+            d6 = currentDate.plusDays(2).toString();
+            d5 = currentDate.plusDays(1).toString();
+            d4 = currentDate.toString();
+            d3 = currentDate.minusDays(1).toString();
+            d2 = currentDate.minusDays(2).toString();
+            d1 = currentDate.minusDays(3).toString();
+        } else if (currentDayOfWeek == DayOfWeek.THURSDAY) {
+            System.out.println("Thursday");
+            d7 = currentDate.plusDays(2).toString();
+            d6 = currentDate.plusDays(1).toString();
+            d5 = currentDate.toString();
+            d4 = currentDate.minusDays(1).toString();
+            d3 = currentDate.minusDays(2).toString();
+            d2 = currentDate.minusDays(3).toString();
+            d1 = currentDate.minusDays(4).toString();
+        } else if (currentDayOfWeek == DayOfWeek.FRIDAY) {
+            System.out.println("Friday");
+            d7 = currentDate.plusDays(1).toString();
+            d6 = currentDate.toString();
+            d5 = currentDate.minusDays(1).toString();
+            d4 = currentDate.minusDays(2).toString();
+            d3 = currentDate.minusDays(3).toString();
+            d2 = currentDate.minusDays(4).toString();
+            d1 = currentDate.minusDays(5).toString();
+        } else { // Saturday
+            System.out.println("Saturday");
+            d7 = currentDate.toString();
+            d6 = currentDate.minusDays(1).toString();
+            d5 = currentDate.minusDays(2).toString();
+            d4 = currentDate.minusDays(3).toString();
+            d3 = currentDate.minusDays(4).toString();
+            d2 = currentDate.minusDays(5).toString();
+            d1 = currentDate.minusDays(6).toString();
+        }
+    }
+    public static void addArray(){
+        String formattedDate = FoodDiaryWindow.formattedDate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate currentDate = LocalDate.parse(formattedDate, formatter);
+        DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
+
+        if (currentDayOfWeek == DayOfWeek.SUNDAY) {
+            calProgressList.add(0, calProgress);
+        } else if (currentDayOfWeek == DayOfWeek.MONDAY) {
+            calProgressList.add(1, calProgress);
+        } else if (currentDayOfWeek == DayOfWeek.TUESDAY) {
+            calProgressList.add(2, calProgress);
+        } else if (currentDayOfWeek == DayOfWeek.WEDNESDAY) {
+            calProgressList.add(3, calProgress);
+        } else if (currentDayOfWeek == DayOfWeek.THURSDAY) {
+            calProgressList.add(4, calProgress);
+        } else if (currentDayOfWeek == DayOfWeek.FRIDAY) {
+            calProgressList.add(5, calProgress);
+        } else {
+            calProgressList.add(6, calProgress);
+        }
     }
 }
